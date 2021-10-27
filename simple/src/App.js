54fb2080dd3a314e76376b5ab344f87bc1e42d58
aurionhoward <aurionhoward@gmail.com>
2021-10-27 19:20:03 -0400

    updates

--- simple/src/App.js
+++ simple/src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect} from "react"
+import ReactDOM from 'react-dom'
+import Home from './Home'
+import About from './About'
+import Under500Cals from "./Under500Cals"
+import { Route, Switch } from "react-router-dom"
+// import logo from './logo.svg';
+// import './App.css';
+import Header from "./Header"
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  const [foods, setFoods] = useState([])
+  console.log(foods)
+  // const [skinnys, setSkinnys] = useState([])
+  useEffect(() => {
+    fetch("http://localhost:3000/recipe")
+    .then(response => response.json())
+    .then(data => setFoods(data))
+    // fetch("http://localhost:3000/skinny")
+    // .then(response =>response.json())
+    // .then(data => setSkinnys(data))
+  },[])
+  console.log(foods,"first")
+  console.log(skinnys,"first")
+
+
+ return (
+    <div>
+      <NavBar />
+      <Switch>
+        <Route exact path="/about">
+          <About />
+        </Route>
+        <Route exact path="/login">
+          <Login />
+        </Route>
+        <Route exact path="/">
+          <Home />
+        </Route>
+      </Switch>
     </div>
   );
 }
 
-export default App;
+export default App
--- simple/src/Under500Cals.js
+++ simple/src/Under500Cals.js
@@ -1,42 +1,12 @@
-import React, { useState } from "react"
-import RecipeList from "./RecipeList";
+import React from 'react'
 
-const[foods, setfoods]= useState ()
+function Under500Cals(){
 
-function Home(){
+    return(
+        <div>
 
-function handleLikes({
-
-})
-
-functionDislikes (){
-
-}
-function handleClick(){
-
-}
-
-return(
-    <li className="cards__items">
-        <div className="card">
-            <img
-              src ={recipe.image}
-              alt={recipe.name}
-              className=
-              onClick={handleClick}
-              />
-              <div className= "card__content">
-                <div className= "card__title">{}</div>
-                <p className="card__text">{? recipe.ingrediants : recipe.instructions}</p>
-                <div className="card__detail">
-                    <p>{recipe.calories}<p>
-                    <p>
-                        Give us a like or dislike if you tried out the meal{" like or dislike button "}
-                    </p>
-                </div>
         </div>
-    </li>
-)
+    )
 }
- 
-export default Home;
+
+export default Under500Cals
